9e5a0723774ba3d1eba6ec15afa1f984a38f0fc3
henriquebortoletto <bortolettohenrique@gmail.com>
2020-03-26 01:01:30 -0300

    front-end da aplicacao pronta

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,47 +2,47 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite' // caminho para o banco de dados
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: './src/database/db.sqlite' // caminho para o banco de dados
+        },
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        useNullAsDefault: true,
     },
-    migrations: {
-      directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
-  },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    staging: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user:     'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    production: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user:     'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     }
-  }
 
 };
