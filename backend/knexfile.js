713a136f73cb03def4cfbc57bab3c2961f7b76be
henriquebortoletto <bortolettohenrique@gmail.com>
2020-03-29 16:15:21 -0300

    testing application

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,6 +13,17 @@ module.exports = {
         useNullAsDefault: true,
     },
 
+    test: {
+        client: 'sqlite3',
+        connection: {
+            filename: './src/database/test.sqlite' // caminho para o banco de dados
+        },
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        useNullAsDefault: true,
+    },
+
     staging: {
         client: 'postgresql',
         connection: {
